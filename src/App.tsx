a7d2dd756091a1ae0c15c0667fa28ed1d1973e74
Jaswant <jaswantkasinedi@gmail.com>
2025-01-02 15:36:55 -0500

    re-implement sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './providers/AuthProvider';
 import { MainLayout } from './components/Layout/MainLayout';
 import { Login } from './pages/Login';
-import { Home } from './pages/Home';
-import { Demo } from './pages/Demo';
-import { Projects } from './pages/Projects';
-import { ProjectDetails } from './pages/ProjectDetails';
-import { JobDetails } from './pages/JobDetails';
-import { Plan } from './pages/account/Plan';
 import { PersonalInfo } from './pages/account/PersonalInfo';
-import { TeamMembers } from './pages/account/TeamMembers';
-import { NDA } from './pages/account/NDA';
-
+import { BlankPage } from './pages/BlankPage';
 function App() {
   return (
     <BrowserRouter>
@@ -19,15 +11,11 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<MainLayout />}>
-            <Route index element={<Home />} />
-            <Route path="demo" element={<Demo />} />
-            <Route path="projects" element={<Projects />} />
-            <Route path="projects/:id" element={<ProjectDetails />} />
-            <Route path="projects/:projectId/jobs/:jobId" element={<JobDetails />} />
-            <Route path="account/plan" element={<Plan />} />
+            <Route index element={<Navigate to="/cables" replace />} />
+            <Route path="cables" element={<BlankPage />} />
+            <Route path="orders" element={<BlankPage />} />
+            <Route path="shipping" element={<BlankPage />} />
             <Route path="account/personal" element={<PersonalInfo />} />
-            <Route path="account/team" element={<TeamMembers />} />
-            <Route path="account/nda" element={<NDA />} />
           </Route>
         </Routes>
       </AuthProvider>
---
+++ src/pages/BlankPage.tsx
@@ -0,0 +1,7 @@
+export const BlankPage = () => {
+    return (
+      <div className="max-w-4xl mx-auto">
+        {/* This page intentionally left blank for future implementation */}
+      </div>
+    );
+  };
