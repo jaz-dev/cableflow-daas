f8c0dab0759254482ac3e7ce6f712b0e3286bfc8
Jaswant <jaswantkasinedi@gmail.com>
2025-01-21 12:31:24 -0500

    define Cable and Quote interfaces

--- src/types/cable.ts
+++ src/types/cable.ts
@@ -12,3 +12,26 @@ export interface CableOverview {
   status: CableStatus;
   created_at: string;
 }
+
+export interface Cable extends CableOverview {
+  cable_description: string;
+  created_by: string;
+  drawing: File;
+  bom: File;
+  from_to_table: File;
+  drawing_modified: boolean;
+  bom_modified: boolean;
+  from_to_table_modified: boolean;
+  quote_table: Quote[];
+  quote_expiration: Date | string;
+  notes: string;
+}
+
+interface Quote {
+  quantity: number;
+  parts_price: number;
+  labor_price: number;
+  unit_price: number;
+  extended_price: number;
+  lead_time: string;
+}
